e1346e4360b74e4525f9d9572c7282c77509ac57
unknown <hanadi.hosri@gmail.com>
2024-03-03 07:47:52 +0200

    displaying dashes

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,5 @@
 const words = ["phone","table","cup","sun","moon","orange","apple","paint","developer","engineer","microphone","speakers","radio","television"];
 const randomIndex = Math.floor(Math.random()*words.length);
 const randomWord = words[randomIndex];
+
+document.getElementById("answer-section").innerHTML = "-".repeat(randomWord.length);
